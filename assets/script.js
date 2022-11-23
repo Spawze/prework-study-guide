e283cb632ccfd533fab079a324b9d3ff24f77fd3
Spawze <riley_allred@hotmail.com>
2022-11-23 14:23:47 -0700

    Added some funky code and logic to the javascript script, and added some notes to the html file

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,34 @@
-var topics = ['Html', 'CSS', 'Git', 'JavaScript']
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[getRandomInt(topics.length)];
 
-for(var x = 0; x < topics.length; x++){
-    console.log(topics[x]);
+function getRandomInt(max){
+    return Math.floor(Math.random() * max)
 }
 
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS")
-} else if (topic === 'Git') {
-    console.log("Let's study Git")
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS")
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript")
-} else {
-    console.log("Please try again! '" + topic + "' isn't a topic!")
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
-*/
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!")
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!")
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!")
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript")
+    } else {
+        console.log("Please try again!")
+    }
+}
+
+console.log('Here are the topics we learned through Prework: ');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
+
